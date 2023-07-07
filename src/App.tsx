caf39706bc39c632c28b8c63a78c9ddcc1296ab4
ziontee113 <ziontee113@gmail.com>
2023-07-07 22:39:36 +0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-stone-900 h-screen text-white flex items-center justify-center">
+      <h1 className="text-8xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
